f6ccb448c63b984d669c2001a78e3349743263f6
Tarchevsky Alexey <swerch.a@gmail.com>
2022-10-20 14:47:20 +0300

    improved messages ui

--- app/src/main/java/swerchansky/recyclers/adapters/MessageAdapter.kt
+++ app/src/main/java/swerchansky/recyclers/adapters/MessageAdapter.kt
@@ -11,6 +11,8 @@ import swerchansky.Constants.USERNAME
 import swerchansky.messenger.FullScreenImageActivity
 import swerchansky.messenger.Message
 import swerchansky.messenger.R
+import swerchansky.messenger.databinding.MyPhotoItemBinding
+import swerchansky.messenger.databinding.MyTextItemBinding
 import swerchansky.messenger.databinding.PhotoItemBinding
 import swerchansky.messenger.databinding.TextItemBinding
 import java.text.DateFormat
@@ -27,25 +29,46 @@ class MessageAdapter(private val context: Context, private val messages: List<Me
       private const val MY_PHOTO = 4
    }
 
-   private val dateFormat: DateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.ENGLISH)
+   private val dateFormat: DateFormat = SimpleDateFormat("HH:mm  dd MMM", Locale.ENGLISH)
 
    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-      if (viewType == TEXT || viewType == MY_TEXT) {
-         return TextViewHolder(
-            TextItemBinding.inflate(
-               LayoutInflater.from(parent.context),
-               parent,
-               false
+      when (viewType) {
+         TEXT -> {
+            return TextViewHolder(
+               TextItemBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+               )
             )
-         )
-      } else {
-         return PhotoViewHolder(
-            PhotoItemBinding.inflate(
-               LayoutInflater.from(parent.context),
-               parent,
-               false
+         }
+         MY_TEXT -> {
+            return MyTextViewHolder(
+               MyTextItemBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+               )
             )
-         )
+         }
+         MY_PHOTO -> {
+            return MyPhotoViewHolder(
+               MyPhotoItemBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+               )
+            )
+         }
+         else -> {
+            return PhotoViewHolder(
+               PhotoItemBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+               )
+            )
+         }
       }
    }
 
@@ -54,53 +77,59 @@ class MessageAdapter(private val context: Context, private val messages: List<Me
       when (getItemViewType(position)) {
          TEXT -> {
             val textHolder = holder as TextViewHolder
-            setTextMessage(textHolder, message)
+            textHolder.text.text = message.data.Text?.text ?: ""
+            textHolder.name.text = message.from
+            textHolder.time.text = dateFormat.format(Date(message.time.toLong()))
          }
          PHOTO -> {
             val photoHolder = holder as PhotoViewHolder
-            setPhotoMessage(photoHolder, message, position)
+            if (message.data.Image?.bitmap == null) {
+               photoHolder.photo.setImageBitmap(
+                  AppCompatResources.getDrawable(
+                     context,
+                     R.drawable.loading
+                  )?.toBitmap(400, 400)
+               )
+            } else {
+               photoHolder.photo.setImageBitmap(message.data.Image.bitmap)
+            }
+            photoHolder.name.text = message.from
+            photoHolder.time.text = dateFormat.format(Date(message.time.toLong()))
+            photoHolder.photoMessage.setOnClickListener {
+               if (message.data.Image?.bitmap != null) {
+                  val intent = Intent(context, FullScreenImageActivity::class.java)
+                  intent.putExtra("messagePosition", position)
+                  context.startActivity(intent)
+               }
+            }
          }
          MY_TEXT -> {
-            val textHolder = holder as TextViewHolder
-            setTextMessage(textHolder, message)
-            textHolder.message.background =
-               AppCompatResources.getDrawable(context, R.drawable.my_message_background)
+            val textHolder = holder as MyTextViewHolder
+            textHolder.text.text = message.data.Text?.text ?: ""
+            textHolder.time.text = dateFormat.format(Date(message.time.toLong()))
          }
          MY_PHOTO -> {
-            val photoHolder = holder as PhotoViewHolder
-            setPhotoMessage(photoHolder, message, position)
-            photoHolder.message.background =
-               AppCompatResources.getDrawable(context, R.drawable.my_message_background)
+            val photoHolder = holder as MyPhotoViewHolder
+            if (message.data.Image?.bitmap == null) {
+               photoHolder.photo.setImageBitmap(
+                  AppCompatResources.getDrawable(
+                     context,
+                     R.drawable.loading
+                  )?.toBitmap(400, 400)
+               )
+            } else {
+               photoHolder.photo.setImageBitmap(message.data.Image.bitmap)
+            }
+            photoHolder.time.text = dateFormat.format(Date(message.time.toLong()))
+            photoHolder.photoMessage.setOnClickListener {
+               if (message.data.Image?.bitmap != null) {
+                  val intent = Intent(context, FullScreenImageActivity::class.java)
+                  intent.putExtra("messagePosition", position)
+                  context.startActivity(intent)
+               }
+            }
          }
       }
-   }
-
-   private fun setPhotoMessage(photoHolder: PhotoViewHolder, message: Message, position: Int) {
-      if (message.data.Image?.bitmap == null) {
-         photoHolder.photo.setImageBitmap(
-            AppCompatResources.getDrawable(
-               context,
-               R.drawable.loading
-            )?.toBitmap(400, 400)
-         )
-      } else {
-         photoHolder.photo.setImageBitmap(message.data.Image.bitmap)
-      }
-      photoHolder.name.text = message.from
-      photoHolder.time.text = dateFormat.format(Date(message.time.toLong()))
-      photoHolder.photoMessage.setOnClickListener {
-         if (message.data.Image?.bitmap != null) {
-            val intent = Intent(context, FullScreenImageActivity::class.java)
-            intent.putExtra("messagePosition", position)
-            context.startActivity(intent)
-         }
-      }
-   }
-
-   private fun setTextMessage(holder: TextViewHolder, message: Message) {
-      holder.text.text = message.data.Text?.text ?: ""
-      holder.name.text = message.from
-      holder.time.text = dateFormat.format(Date(message.time.toLong()))
    }
 
    override fun getItemCount() = messages.size
@@ -129,10 +158,23 @@ class MessageAdapter(private val context: Context, private val messages: List<Me
       val time = messageItemBinding.time
    }
 
+   class MyTextViewHolder(messageItemBinding: MyTextItemBinding) :
+      RecyclerView.ViewHolder(messageItemBinding.root) {
+      val text = messageItemBinding.text
+      val time = messageItemBinding.time
+   }
+
    class PhotoViewHolder(messageItemBinding: PhotoItemBinding) :
       RecyclerView.ViewHolder(messageItemBinding.root) {
       val message = messageItemBinding.photoMessage
       val name = messageItemBinding.name
+      val photo = messageItemBinding.photo
+      val time = messageItemBinding.time
+      val photoMessage = messageItemBinding.photoMessage
+   }
+
+   class MyPhotoViewHolder(messageItemBinding: MyPhotoItemBinding) :
+      RecyclerView.ViewHolder(messageItemBinding.root) {
       val photo = messageItemBinding.photo
       val time = messageItemBinding.time
       val photoMessage = messageItemBinding.photoMessage
